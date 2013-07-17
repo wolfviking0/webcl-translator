ad2d9dcd9a247651097b093482e1b074cc045e9b
Anthony Liot <anthony.liot@gmail.com>
2013-07-17 14:16:03 +0200

    Just some other test

--- webcl/qjulia/qjulia_kernel.js
+++ webcl/qjulia/qjulia_kernel.js
@@ -43,7 +43,7 @@ function assert(check, msg) {
     var PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
     var PACKAGE_NAME = 'qjulia_kernel.data';
     var REMOTE_PACKAGE_NAME = 'qjulia_kernel.data';
-    var PACKAGE_UUID = '47f67202-bca1-4fad-ae0d-d806c5751be1';
+    var PACKAGE_UUID = '02b641f9-4f30-44f6-b540-8911a908bc6f';
   
     function fetchRemotePackage(packageName, callback, errback) {
       var xhr = new XMLHttpRequest();
