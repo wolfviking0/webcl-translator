09e4ea12f026ec600dd01d8fd8dcd50f3a046e03
Anthony Liot <anthony.liot@gmail.com>
2014-01-04 20:13:15 +0100

    New particles sample

---
+++ webcl/particles/util.h
@@ -0,0 +1,17 @@
+// createVBO routine and my first experience with VBOs from:
+// http://www.songho.ca/opengl/gl_vbo.html
+
+#ifndef ADVCL_UTIL_H_INCLUDED
+#define ADVCL_UTIL_H_INCLUDED
+
+char *file_contents(const char *filename, int *length);
+
+const char* oclErrorString(cl_int error);
+
+//create a VBO
+//target is usually GL_ARRAY_BUFFER 
+//usage is usually GL_DYNAMIC_DRAW
+GLuint createVBO(const void* data, int dataSize, GLenum target, GLenum usage);
+
+
+#endif
