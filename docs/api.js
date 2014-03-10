03755df274530c1275f6fe1961b4cc844bec31c3
Anthony Liot <anthony.liot@gmail.com>
2014-03-10 20:33:04 +0100

    update docs

--- docs/api.js
+++ docs/api.js
@@ -1,10 +1,24 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "CL",
+        "CU"
+    ],
+    "modules": [
         "LibraryCUDA",
         "LibraryOpenCL"
     ],
-    "modules": [],
-    "allModules": []
+    "allModules": [
+        {
+            "displayName": "LibraryCUDA",
+            "name": "LibraryCUDA",
+            "description": "library_cuda.js\nLicence : https://github.com/wolfviking0/webcl-translator/blob/master/LICENSE\n\nCreated by Anthony Liot.\nCopyright (c) 2013 Anthony Liot. All rights reserved."
+        },
+        {
+            "displayName": "LibraryOpenCL",
+            "name": "LibraryOpenCL",
+            "description": "library_opencl.js\nLicence : https://github.com/wolfviking0/webcl-translator/blob/master/LICENSE\n\nCreated by Anthony Liot.\nCopyright (c) 2013 Anthony Liot. All rights reserved."
+        }
+    ]
 } };
 });
